9167b5aad7a3ee3bc60036ccb9487131f662949a
SEdwards <sanchez_e@hotmail.co.uk>
2019-06-17 21:11:25 +0100

    can get all values, but what about keys with two values?

--- src/main/java/com/spartaglobal/App.java
+++ src/main/java/com/spartaglobal/App.java
@@ -6,14 +6,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        //JacksonManual manual = new JacksonManual("resources/postcode.json");
         PostcodeDeserialiser postcodeDeserialiser = new PostcodeDeserialiser("resources/postcode.json");
         System.out.println(postcodeDeserialiser.postcodesMapped.getStatus());
-        System.out.println(postcodeDeserialiser.postcodesMapped.getResult().get("postcode"));
-        System.out.println(postcodeDeserialiser.postcodesMapped.getResult().get("eastings"));
-        Map listCode = (Map) postcodeDeserialiser.postcodesMapped.getResult().get("codes");
-        System.out.println(listCode.get("parish"));
+
+        System.out.println(postcodeDeserialiser.postcodesMapped.getPostcode());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getQuality());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getEastings());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getNorthings());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getCountry());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getNhs_ha());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getLongitude());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getLatitude());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getEuropean_electoral_region());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getPrimary_care_trust());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getRegion());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getLsoa());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getMsoa());
+
+        System.out.println(postcodeDeserialiser.postcodesMapped.getAdmin_district());
         System.out.println(postcodeDeserialiser.postcodesMapped.getAdmin_county());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getAdmin_ward());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getParish());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getParliamentary_constituency());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getCcg());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getCed());
+        System.out.println(postcodeDeserialiser.postcodesMapped.getNuts());
+
+
+
+
+
 
     }
 }
--- src/main/java/com/spartaglobal/PostcodeDTO.java
+++ src/main/java/com/spartaglobal/PostcodeDTO.java
@@ -14,6 +14,7 @@ public class PostcodeDTO {
     private String admin_district;
     private String admin_ward;
     private double longitude;
+    private double latitude;
     private String parliamentary_constituency;
     private String european_electoral_region;
     private String primary_care_trust;
@@ -25,8 +26,14 @@ public class PostcodeDTO {
     private String ccg;
     private String nuts;
     private Map<String, Object> result;
-    private Map<String, Integer> quality;
-    //private Map<Object, >
+    private int quality;
+    private Map codes;
+
+    public Map getCodes() {
+        Map listResult = result;
+        codes =(Map)listResult.get("codes");
+        return codes;
+    }
 
     public int getStatus() {
         return status;
@@ -36,86 +43,108 @@ public class PostcodeDTO {
         return result;
     }
 
-    public String getAdmin_county() {
-        Map listResult = result;
-         Map listAdmin =(Map)listResult.get("codes");
-         admin_county = (String) listAdmin.get("admin_county");
-        return admin_county;
-    }
-
-    public String getAdmin_district() {
-        return admin_district;
-    }
-
-    public String getAdmin_ward() {
-        return admin_ward;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public Map<String, Integer> getQuality() {
-        return quality;
-    }
-
-    public String getParliamentary_constituency() {
-        return parliamentary_constituency;
-    }
-
-    public String getEuropean_electoral_region() {
-        return european_electoral_region;
-    }
-
-    public String getPrimary_care_trust() {
-        return primary_care_trust;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public String getParish() {
-        return parish;
-    }
-
-    public String getLsoa() {
-        return lsoa;
-    }
-
-    public String getMsoa() {
-        return msoa;
-    }
-
-    public String getCed() {
-        return ced;
-    }
-
-    public String getCcg() {
-        return ccg;
-    }
-
-    public String getNuts() {
-        return nuts;
-    }
-
     public String getPostcode() {
+        postcode = (String) getResult().get("postcode");
         return postcode;
     }
 
+    public int getQuality() {
+        quality = (Integer) getResult().get("quality");
+        return quality;
+    }
+
     public int getEastings() {
+        eastings = (Integer) getResult().get("eastings");
         return eastings;
     }
 
     public int getNorthings() {
+        northings = (Integer) getResult().get("northings");
         return northings;
     }
 
     public String getCountry() {
+        country = (String) getResult().get("country");
         return country;
     }
 
     public String getNhs_ha() {
+        nhs_ha = (String) getResult().get("nhs_ha");
         return nhs_ha;
     }
+
+    public double getLongitude() {
+        longitude = (double) getResult().get("longitude");
+        return longitude;
+    }
+
+    public double getLatitude() {
+        latitude = (double) getResult().get("latitude");
+        return latitude;
+    }
+
+    public String getEuropean_electoral_region() {
+        european_electoral_region = (String) getResult().get("european_electoral_region");
+        return european_electoral_region;
+    }
+
+    public String getPrimary_care_trust() {
+        primary_care_trust = (String) getResult().get("primary_care_trust");
+        return primary_care_trust;
+    }
+
+    public String getRegion() {
+        region = (String) getResult().get("region");
+        return region;
+    }
+
+    public String getLsoa() {
+        lsoa = (String) getResult().get("lsoa");
+        return lsoa;
+    }
+
+    public String getMsoa() {
+        msoa = (String) getResult().get("msoa");
+        return msoa;
+    }
+
+    public String getAdmin_district() {
+        admin_district = (String) getCodes().get("admin_district");
+        return admin_district;
+    }
+
+    public String getAdmin_county() {
+         admin_county = (String) getCodes().get("admin_county");
+        return admin_county;
+    }
+
+    public String getAdmin_ward() {
+        admin_ward = (String) getCodes().get("admin_ward");
+        return admin_ward;
+    }
+    public String getParish() {
+        parish = (String) getCodes().get("parish");
+        return parish;
+    }
+
+    public String getParliamentary_constituency() {
+        parliamentary_constituency = (String) getCodes().get("parliamentary_constituency");
+        return parliamentary_constituency;
+    }
+
+    public String getCcg() {
+        ccg = (String) getCodes().get("ccg");
+        return ccg;
+    }
+
+    public String getCed() {
+        ced = (String) getCodes().get("ced");
+        return ced;
+    }
+
+    public String getNuts() {
+        nuts = (String) getCodes().get("nuts");
+        return nuts;
+    }
+
 }
